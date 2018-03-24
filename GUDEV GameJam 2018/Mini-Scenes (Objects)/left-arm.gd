187e94220f7756f546945c90169600ecd1b4f0ab
Iain Cattermole <iain_c@live.co.uk>
2018-03-24 12:16:27 +0000

    buggy boi

---
+++ GUDEV GameJam 2018/Mini-Scenes (Objects)/left-arm.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var rotation_speed = 50
+var openRot = 70
+var closedRot = 20
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	var rot = self.rotation_degrees
+	if Input.is_key_pressed(KEY_SPACE):
+		rot += delta * -rotation_speed
+		if rot < closedRot:
+			rot = closedRot
+	else:
+		rot += delta * rotation_speed
+		if rot > openRot:
+			rot = openRot
+	
+	self.rotation_degrees = rot
+		
